94f3ad059dead6734b94d9a315f88196631cf0ad
Franco Lepe <Francolepe169@gmail.com>
2023-09-27 16:30:54 -0400

    arrow home button
    hopfully fixes

--- over_night/src/components/Login_signUp.tsx
+++ over_night/src/components/Login_signUp.tsx
@@ -4,42 +4,58 @@ import axios from "axios";
 import background from "../assets/miami.jpeg"
 import Login from "./Login"
 import SignUp from "./SignUp";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from "framer-motion";
+import homeButton from "../assets/arrow.png"
+import { NavLink } from 'react-router-dom';
+import Navbar from "./NavBar";
 
 export default function Login_signUp() {
-    const y = (x : any) : void => {setState(x) ; console.log("hi")}
 
-    const [isLogin, setIsLogin] = useState<boolean>(true)
-    // const [isVisible, setIsVisible] = useState(true);
+  const [isLogin, setIsLogin] = useState<boolean>(true)
+  const [initialRender, setInitialRender] = useState<boolean>(true)
+  // const [isVisible, setIsVisible] = useState(true);
 
-    const setState = (x : boolean) : void => {
-        setIsLogin(x)
+  const setState = (x : boolean) : void => {
+      setIsLogin(x)
 
-    }
+  }
 
-    return (
+  useEffect(() : void => {
+    setInitialRender(false)
+  },[])
 
-        <div className="w-full h-screen bg-cover bg-black" style={{backgroundImage: `url(${background})`}} >
-        <div className=" flex justify-center items-center   bg-black bg-opacity-30 w-full h-full">
 
-        <AnimatePresence mode='wait'>
-          <motion.div className=''
-            key={isLogin}
-            initial={{ x: 20, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            exit={{ x: -20, opacity: 0 }}
-            transition={{ duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96]  }}
-            > 
-           {isLogin ? <Login setState={setState}/> : <SignUp setState={setState}/>}
-          </motion.div> 
-        </AnimatePresence>
+  return (
+
+      <div className="w-full h-screen bg-cover bg-black" style={{backgroundImage: `url(${background})`}} >
+      <div className=" flex justify-center items-center relative  bg-black bg-opacity-30 w-full h-full">
+       <NavLink to= "/">
+      <motion.img className="h-10 absolute left-0 top-0" src={homeButton}
+      whileHover={{
+        scale: 1.2,
+        transition: { duration: 1 },
         
-        </div>
-        </div>
-        
-    
-        
+      }}/>
+      </NavLink>
+      <AnimatePresence mode='wait'>
+        <motion.div className=''
 
-    )
+          key={isLogin}
+          initial={initialRender ? {} : { x: 20, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: -20, opacity: 0 }}
+          transition={{ duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96]  }}
+          > 
+         {isLogin ? <Login setState={setState}/> : <SignUp setState={setState}/>}
+        </motion.div> 
+      </AnimatePresence>
+      
+      </div>
+      </div>
+      
+  
+      
+
+  )
 }
--- over_night/src/components/NavBar.tsx
+++ over_night/src/components/NavBar.tsx
@@ -3,11 +3,12 @@ import logo from '../assets/overnightlogo.png'
 import SignUp from './SignUp'
 import LogOut from './LogOut'
 import { useSelector } from "react-redux"
+import Navbar from '../assets/Navbar.png'
 
 export default function NavBar(){
 const user = useSelector((state: any) => state.user.value)
     return(
-        <div className="flex justify-center h-30 border border-red-900">
+        <div className="flex justify-center border h-20  bg-center bg-contain bg-no-repeat mb-10" style={{backgroundImage: `url(${Navbar})`}}>
             <NavLink className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to= "/signup">Sign Up</NavLink>
           <a><img src={logo} className="h-20" alt="logo" /></a>
             {user == null ? <NavLink className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" to="/login" >Login</NavLink> : <LogOut/>}
