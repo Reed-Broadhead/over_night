2d267bf016024f1a287865f07e972d39790265f5
Franco Lepe <Francolepe169@gmail.com>
2023-09-28 15:09:23 -0400

    homepage styling issues

--- over_night/src/components/NavBar.tsx
+++ over_night/src/components/NavBar.tsx
@@ -1,21 +1,43 @@
 import { NavLink } from 'react-router-dom'
-import logo from '../assets/whiteLogo.png'
+import logo from '../assets/overnightlogo.png'
 import SignUp from './SignUp'
 import LogOut from './LogOut'
 import { useSelector } from "react-redux"
+import Dots from "../assets/Dots.png"
+import { Menu, Transition } from "@headlessui/react";
 
 
 export default function NavBar(){
 const user = useSelector((state: any) => state.user.value)
     return(
-        <div className=" bg-transparent flex border h-30 bg-contain bg-no-repeat mb-10 text-right">
-             
+
+        <div className=" bg-transparent flex  h-30 w-full bg-contain bg-no-repeat mb-10 text-right">
              <div className='w-1/2'>
                 <a><img src={logo} className="pt-1 pl-1 h-14" alt="logo" /></a>
             </div>
-             
-            <div className='w-1/2'>
-                {user == null ? <NavLink className=" border mt-1 right-0 hover:bg-logos-blue text-white font-bold py-2 px-4 rounded" to="/login" >Login</NavLink> : <LogOut/>}
+            <div className='w-1/2 flex flex-row-reverse mr-2 '>
+
+
+        <Menu as='div' className="relative" >
+
+            <Menu.Button>
+            <img src={Dots} className='h-1.5 mt-5'/>
+            </Menu.Button>
+
+                <Menu.Items className="absolute right-0 border border-gray-700 rounded-sm  mt-2 shadow-md ">               
+                <Menu.Items className=" w-28 text-white bg-gray-600 bg-opacity-10 hover:bg-gray-400 border-y border-y-gray-600 rounded-t-sm flex justify-center ">FAQ</Menu.Items>
+                <Menu.Items className=" w-28 text-white bg-gray-600 bg-opacity-10 hover:bg-gray-400 border-y border-y-gray-600 rounded-t-sm flex justify-center ">About Us</Menu.Items>
+                </Menu.Items>
+            
+        </Menu>
+
+
+
+
+                {user == null ? 
+                <NavLink className="mt-1.5 mr-3 h-fit hover:bg-logos-blue text-white text-2xl " to="/login" >Login</NavLink>
+                 : <LogOut/>}
+                 
             </div>
             
         </div>
--- over_night/src/main.tsx
+++ over_night/src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
    <React.StrictMode>
      <App />
-    </React.StrictMode>,
+    </React.StrictMode>
   </BrowserRouter>
   </Provider>
 )
