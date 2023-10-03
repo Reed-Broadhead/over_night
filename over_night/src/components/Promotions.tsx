7a93c3960a029e4de0e03c839138d677bdba460a
Franco Lepe <Francolepe169@gmail.com>
2023-10-03 12:58:04 -0400

    styled promotions section

--- over_night/src/components/Promotions.tsx
+++ over_night/src/components/Promotions.tsx
@@ -3,9 +3,17 @@
 export default function Promotions() {
 
     return (
-        <div className="flex  items-center justify-center bg-white w-full h-[600px]">
-            <h1> In Production </h1>
+        <div>
+            <h1 className="flex ml-24 mt-24 text-3xl  h-min w-[575px] flex items-center rounded border-b-4 border-logos-blue">Get the Best Deals, For Over Night Bookings</h1>
+        
+            <div className="flex items-center justify-center bg-white w-full h-[600px] grid grid-cols-4">
+            
+                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white rounded-xl shadow-2xl"></div>
+                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white rounded-xl shadow-2xl"></div>
+                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white rounded-xl shadow-2xl"></div>
+                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white rounded-xl shadow-2xl"></div>
 
+        </div>
         </div>
     )
 }
