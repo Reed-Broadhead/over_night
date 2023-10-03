d96af9986e4a7fb86f7c5549f98b362cc504f88d
Franco Lepe <Francolepe169@gmail.com>
2023-10-03 13:01:41 -0400

    styled hover on promotions section

--- over_night/src/components/Promotions.tsx
+++ over_night/src/components/Promotions.tsx
@@ -4,14 +4,15 @@ export default function Promotions() {
 
     return (
         <div>
-            <h1 className="flex ml-24 mt-24 text-3xl  h-min w-[575px] flex items-center rounded border-b-4 border-logos-blue">Get the Best Deals, For Over Night Bookings</h1>
+            <h1 className="flex ml-24 mt-24 text-3xl  h-min w-[575px] flex items-center rounded border-b-4 border-logos-blue"
+            >Get the Best Deals, For Over Night Bookings</h1>
         
             <div className="flex items-center justify-center bg-white w-full h-[600px] grid grid-cols-4">
             
-                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white rounded-xl shadow-2xl"></div>
-                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white rounded-xl shadow-2xl"></div>
-                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white rounded-xl shadow-2xl"></div>
-                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white rounded-xl shadow-2xl"></div>
+                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white hover:bg-gray-100 rounded-xl shadow-2xl"></div>
+                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white hover:bg-gray-100 rounded-xl shadow-2xl"></div>
+                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white hover:bg-gray-100 rounded-xl shadow-2xl"></div>
+                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white hover:bg-gray-100 rounded-xl shadow-2xl"></div>
 
         </div>
         </div>
