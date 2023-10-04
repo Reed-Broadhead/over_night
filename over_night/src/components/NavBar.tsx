20c11229f6e23bc600a9bb8ba9be42324b30375b
Franco Lepe <Francolepe169@gmail.com>
2023-10-04 10:43:19 -0400

    gray navbar

--- over_night/src/components/NavBar.tsx
+++ over_night/src/components/NavBar.tsx
@@ -1,88 +1,77 @@
-import { NavLink } from 'react-router-dom'
-import Blacklogo from '../assets/blacklogo.png'
-import Whitelogo from "../assets/whiteLogo.png"
-import SignUp from './SignUp'
-import LogOut from './LogOut'
-import { useSelector } from "react-redux"
-import Dots from "../assets/whitedots.png"
-import { Menu, Transition } from "@headlessui/react";
-import Underline from './Underline'
+import { NavLink } from 'react-router-dom';
+import Blacklogo from '../assets/blacklogo.png';
+import Whitelogo from '../assets/whiteLogo.png';
+import SignUp from './SignUp';
+import LogOut from './LogOut';
+import { useSelector } from 'react-redux';
+import Dots from '../assets/whitedots.png';
+import { Menu } from '@headlessui/react';
+import Underline from './Underline';
 import { useEffect, useState } from 'react';
 
-//  absolute top-0 left-0
-export default function NavBar(){
-const user = useSelector((state: any) => state.user.value)
-const [scrolling, setScrolling] = useState(false);
+export default function NavBar() {
+  const user = useSelector((state: any) => state.user.value);
+  const [scrolling, setScrolling] = useState(false);
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 0) {
-                setScrolling(true);
-            } else {
-                setScrolling(false);
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
 
-        window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll);
 
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-     }, []);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-
-    return(
-        
-        <div className={`bg-transparent flex h-15 w-full bg-contain bg-no-repeat fixed z-10 text-right ${
-            scrolling ? 'bg-gray-700 bg-opacity-30 ' : '' }`}>
-             
-             <div className='w-1/2'>
-                
-                <div className='ml-1'>
-                    <Underline>
-                        <img src={Whitelogo} className=" pt-1 pl-1 h-14" alt="logo" />
-                    </Underline>
-                </div>
-
-            </div>
-            <div className='w-1/2 flex flex-row-reverse mr-2 '>
-
-
-        <Menu as='div' className="relative" >
-
-            <Menu.Button>
-            <img src={Dots} className='h-1.5 mt-5'/>
-            </Menu.Button>
-
-                <Menu.Items className="absolute right-0 border border-gray-700 rounded-sm  mt-2 shadow-md ">               
-                <Menu.Items className=" w-28 text-white bg-gray-600 bg-opacity-10 hover:bg-gray-400 border-y border-y-gray-600 rounded-t-sm flex justify-center ">FAQ</Menu.Items>
-                <Menu.Items className=" w-28 text-white bg-gray-600 bg-opacity-10 hover:bg-gray-400 border-y border-y-gray-600 rounded-t-sm flex justify-center ">About Us</Menu.Items>
-                </Menu.Items>
-            
-        </Menu>
-
-
-
-
-                {user == null ? 
-                <div className='mr-3 mt-1.5' >
-                <Underline underlineRight={true} >
-                    <NavLink className=" h-fit hover:text-gray-100 text-white text-2xl " to="/login" >Login</NavLink>
-                </Underline>
-                </div>
-                 : 
-                 <div className='mr-3 mt-1.5' >
-                 <Underline underlineRight={true} >
-                 <LogOut/>
-                 </Underline>
-                </div>
-                 
-                 }
-                 
-            </div>
-            
+  return (
+    <div
+      className={`flex h-15 w-full bg-contain bg-no-repeat fixed z-10 text-right ${
+        scrolling ? 'bg-gray-700 bg-opacity-30' : ''
+      }`}
+    >
+      <div className="w-1/2">
+        <div className="ml-1">
+          <Underline>
+            <img src={Whitelogo} className="pt-1 pl-1 h-14" alt="logo" />
+          </Underline>
         </div>
-       
-    )
+      </div>
+      <div className="w-1/2 flex flex-row-reverse mr-2">
+        <Menu as="div" className="relative">
+          <Menu.Button>
+            <img src={Dots} className="h-1.5 mt-5" />
+          </Menu.Button>
+          <Menu.Items className="absolute right-0 border border-gray-700 rounded-sm mt-2 shadow-md">
+            <Menu.Items className="w-28 text-white bg-gray-600 bg-opacity-10 hover:bg-gray-400 border-y border-y-gray-600 rounded-t-sm flex justify-center">
+              FAQ
+            </Menu.Items>
+            <Menu.Items className="w-28 text-white bg-gray-600 bg-opacity-10 hover:bg-gray-400 border-y border-y-gray-600 rounded-t-sm flex justify-center">
+              About Us
+            </Menu.Items>
+          </Menu.Items>
+        </Menu>
+        {user == null ? (
+          <div className="mr-3 mt-1.5">
+            <Underline underlineRight={true}>
+              <NavLink className="h-fit hover:text-gray-100 text-white text-2xl" to="/login">
+                Login
+              </NavLink>
+            </Underline>
+          </div>
+        ) : (
+          <div className="mr-3 mt-1.5">
+            <Underline underlineRight={true}>
+              <LogOut />
+            </Underline>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
-
