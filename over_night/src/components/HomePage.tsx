161201d7f33df20d86e58ddfa2f2a45dc68edccb
Franco Lepe <Francolepe169@gmail.com>
2023-10-09 15:21:12 -0400

    promo blocks

--- over_night/src/components/HomePage.tsx
+++ over_night/src/components/HomePage.tsx
@@ -4,18 +4,58 @@ import Promotions from "./Promotions"
 import promotional from "../assets/Dallas.png"
 import PopularStays from "./PopularStays"
 import Footer from "./Footer"
+import { useState, useEffect } from "react"
+import passwordimage from '../assets/password.png'
+
 
 export default function HomePage()  {
 
+    const [sixStars, setSixStars] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/sixstars')
+          .then((resp) => resp.json())
+          .then((sixStarHOtelData) => setSixStars(sixStarHOtelData));
+      }, []);
+
+      const fakeData=[
+        {name:"joe",
+        id:1,
+        image:passwordimage},
+
+        {name:"joe",
+        id:2,
+        image:passwordimage},
+
+        {name:"yay",
+        id:3,
+        image:passwordimage},
+
+        {name:"you",
+        id:4,
+        image:passwordimage},
+
+    ]
+
+   const locks= fakeData.map((element, index) => (
+        <Promotions  key={index} name={element.name} id={element.id} image={element.image}/>
+      ))
+        
     return (
         <>
-        
         <NavBar/>
-        
 
         <HeadPage/>
-        <Promotions/>
-        
+
+            <div>
+            <h1 className="flex ml-24 mt-24 text-3xl  h-min w-[575px] flex items-center rounded border-b-4 border-logos-blue"
+            >Get the Best Deals, For Over Night Bookings</h1>
+            <div 
+                className="flex items-center justify-center bg-white w-full h-[600px] grid grid-cols-4">
+                {locks}
+            </div>
+            </div>
+       
         <div  
             className="flex justify-center items-center w-full h-[600px]"
             style={{
--- over_night/src/components/Promotions.tsx
+++ over_night/src/components/Promotions.tsx
@@ -1,20 +1,20 @@
+import React from "react"
 
 
-export default function Promotions() {
+
+
+export default function Promotions({name,id,image}){
+    
+
 
     return (
-        <div>
-            <h1 className="flex ml-24 mt-24 text-3xl  h-min w-[575px] flex items-center rounded border-b-4 border-logos-blue"
-            >Get the Best Deals, For Over Night Bookings</h1>
-        
-            <div className="flex items-center justify-center bg-white w-full h-[600px] grid grid-cols-4">
+       
             
-                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white hover:bg-gray-100 rounded-xl shadow-2xl"></div>
-                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white hover:bg-gray-100 rounded-xl shadow-2xl"></div>
-                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white hover:bg-gray-100 rounded-xl shadow-2xl"></div>
-                <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white hover:bg-gray-100 rounded-xl shadow-2xl"></div>
+            
+        <div className=" ml-12 mr-12 h-[350px] w-[350px]  bg-white hover:bg-gray-100 rounded-xl shadow-2xl">{name}{id}<img src ={image}/></div>
+            
 
-        </div>
-        </div>
+       
+       
     )
 }
