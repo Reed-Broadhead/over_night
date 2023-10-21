624f07ad11c6d0b76d9cc773031ec4c7d5ba35af
Reed <reedbroadhead@gmail.com>
2023-10-21 09:29:14 -0500

    adds data for footer

--- over_night/src/components/Footer.tsx
+++ over_night/src/components/Footer.tsx
@@ -7,7 +7,7 @@ import medium from "../assets/medium.png"
 
 export default function Footer(){
 
-    const elements = [["instagram", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["10", "11", "12"]]
+    const elements = [["about", "login", "sign up"], ["FAQ", "Privacy Policy", "Help"], ["Flight", "Stays", "Cars"], ["Linkedin", "GitHub", "Medium"]]
 
     const mappedFooterSection = elements.map((el) => {
         return (
@@ -16,39 +16,27 @@ export default function Footer(){
 
     return(
         <div className="border h-[340px] flex flex-row border-t border-gray-700">
-            
-            
+
             {mappedFooterSection}
-            <div className="w-[28%] border flex flex-col-reverse">
+
+            <div className="w-[28%]  flex flex-col-reverse">
+
                 <h1 className="mb-8 ml-10 text-gray-500">Copyright ©️ 2023. All Rights Reserved.</h1>
+
                 <img className="mb-16" src={logo}/>
+
                 <div className="ml-8 mb-8 w-2/4 flex flex-row  ">
-
-                    <div className="w-12 mx-auto shadow-lg rounded-full hover:shadow-xl">
-                        <motion.img 
-                        className="" 
-                        src={linkedin}
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.9 }}
-                        ></motion.img>
-                    </div>
-
-                    <div className="w-12 mx-auto shadow-lg rounded-full hover:shadow-xl">
-                        <motion.img 
-                        className="" 
-                        src={github}
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.9 }}></motion.img>
-                    </div>
-
-                    <div className="w-12 mx-auto shadow-lg rounded-full hover:shadow-xl">
-                        <motion.img 
-                        className="" 
-                        src={medium}
-                        whileHover={{ scale: 1.2 }}
-                        whileTap={{ scale: 0.9 }}></motion.img>
-                    </div>
+                    {[linkedin, github, medium].map((el) => {
+                        return (
+                            <div className="w-12 mx-auto shadow-lg rounded-full hover:shadow-xl">
+                                <motion.img 
+                                src={el}
+                                whileHover={{ scale: 1.2 }}
+                                whileTap={{ scale: 0.9 }}/>
+                            </div>)
+                    })}
                 </div>
+
             </div>
 
         </div>
