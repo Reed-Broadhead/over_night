030d772b8693d01e2f7b99796486d417cd8c9175
Reed <reedbroadhead@gmail.com>
2023-10-30 14:41:55 -0500

    adds more to about section

--- over_night/src/components/About.tsx
+++ over_night/src/components/About.tsx
@@ -13,39 +13,68 @@ export default function About(){
             <div className="h-36 bg-gray-900" />
             <div className="bg-white flex relative w-full h-3/4 bg-cover flex flex-col items-center" >
             
-            <div className=" flex flex-row h-full w-full">
-                <div className ='text-2xl h-full w-1/3  text-center  text-black flex items-center '>
+            <div className=" flex flex-row justify-center h-full w-full border border-red-800 px-20">
+
+                <div className ='text-2xl h-full w-[400px] flex flex-col text-black flex mt-10 '>
+                    <h1 className="text-bold text-2xl w-[400px] text-center">
                     MEET THE LINE UP
-                </div>
-                <div className="flex flex-row h-full w-2/3"> 
-                
-                    <div className="w-full h-full  ">
-                        <div className="h-[650px] w-[500px] p-2 ">
-                            <img className="h-[440px] w-[500px] fit " src={dallas}></img>
-                        <h2 className=" text text-2xl mt-3 ">Reed Block</h2>
-                        <h2 className=" text text-xl  text-indigo-600">Lead Engineer</h2>
-                        <p className="text-gray-500 mt-2">you yo sjdf askksj  hhhhhhhhhhhhhhhhhhhhjsjjs jkfgdsfds ghkjtr cvtbbb bbubuub jjjjj</p>
-
-
-                        <div className="mt-4 w-1/3 flex flex-row  ">
-                             {[linkedin, github, medium].map((el) => {
-                                 return (
-                            <div to="/about" className="w-10 mx-auto  rounded-full hover:shadow-xl">
-                            
-                                <motion.img 
-                                src={el}
-                                whileHover={{ scale: 1.2 }}
-                                whileTap={{ scale: 0.9 }}/>
-                           
-
-                            </div>)
-                    })}
+                    </h1>
+                    <h1 className="mt-5 text-xl w-[400px] ">
+                        A couple of full stack software developers helping you see the world
+                    </h1>
                 </div>
 
+                <div className="flex flex-row h-full w-2/3  mt-10"> 
+                    <div className="w-full h-full flex flex-row  ">
+                {/* first / reed  */}
+
+                        <div className="h-[650px] w-[400px]  mr-10">
+                            <img className="h-[400px] w-[400px] fit " src={dallas}></img>
+                            <h2 className=" text text-2xl mt-3 ml-1 ">Reed Broadhead</h2>
+                            <h2 className=" text text-xl  text-indigo-600 ml-1">Lead Engineer</h2>
+                            <p className="text-gray-500 mt-2 ml-1">you yo sjdf askksj  hhhhhhhhhhhhhhhhhhhhjsjjs jkfgdsfds ghkjtr cvtbbb bbubuub jjjjj</p>
+
+
+                            <div className="mt-4 w-1/3 flex flex-row  ">
+                                {[linkedin, github, medium].map((el) => {
+                                        return (
+                                    <div to="/about" className="w-10 mx-auto  rounded-full hover:shadow-xl">
+                                    
+                                        <motion.img 
+                                        src={el}
+                                        whileHover={{ scale: 1.2 }}
+                                        whileTap={{ scale: 0.9 }}/>
+                                
+
+                                    </div>
+                                )})}
+                            </div>  
                         </div>
-                        
+                        {/*  second / franco */}
+                        <div className="h-[650px] w-[400px]  ">
+                            <img className="h-[400px] w-[400px] fit " src={dallas}></img>
+                            <h2 className=" text text-2xl mt-3 ">Franco Lepe</h2>
+                            <h2 className=" text text-xl  text-indigo-600">Lead Engineer</h2>
+                            <p className="text-gray-500 mt-2">you yo sjdf askksj  hhhhhhhhhhhhhhhhhhhhjsjjs jkfgdsfds ghkjtr cvtbbb bbubuub jjjjj</p>
 
+
+                            <div className="mt-4 w-1/3 flex flex-row  ">
+                                {[linkedin, github, medium].map((el) => {
+                                        return (
+                                    <div to="/about" className="w-10 mx-auto  rounded-full hover:shadow-xl">
+                                    
+                                        <motion.img 
+                                        src={el}
+                                        whileHover={{ scale: 1.2 }}
+                                        whileTap={{ scale: 0.9 }}/>
+                                
+
+                                    </div>
+                                )})}
+                            </div>  
+                        </div>
                     </div>
+{/* 
                     <div className="w-full h-full border">
                         <div className="h-[650px] w-[500px] border">
                         <img className="h-[440px] w-[500px] fit border border-red-400"></img>
@@ -54,14 +83,14 @@ export default function About(){
                         </div>
                         
 
-                    </div>
+                    </div> */}
 
                 </div>
             </div >
             <div className="border ">
 
                 <div className="h-[270px] w-[500px] border">
-                    Adyan
+                    Adyan. she helped!
                 </div>
             </div>
             </div>
