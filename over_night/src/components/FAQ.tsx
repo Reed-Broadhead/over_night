fd19d02fbbc73a95b9c352a90d0fd8a0a812c0e6
Reed <reedbroadhead@gmail.com>
2023-11-16 13:13:34 -0600

    adds questions to FAQ

--- over_night/src/components/FAQ.tsx
+++ over_night/src/components/FAQ.tsx
@@ -8,10 +8,10 @@ export default function FAQ(){
 
 // data to use
     const data = [
-    {question: "What Tech stack was used to build this app?", answer: " Over Night was built using react with typescript for the front-end, and express for the back-end "}, 
-    {question: "What Tech stack was used to build this app?", answer: " Over Night was built using react with typescript for the front-end, and express for the back-end "},
-    {question: "What Tech stack was used to build this app?", answer: " Over Night was built using react with typescript for the front-end, and express for the back-end "},
-    {question: "What Tech stack was used to build this app?", answer: " Over Night was build using react with typescript for the front-end, and express for the back-end "},
+    {question: "Can I book hotels on OverNight?", answer: "Booking functionality is currently not supported on OverNight; however, it is under development and is expected to be added soon."}, 
+    {question: "What happens when I try to book on OverNight?", answer: "You will be redirected to the designated hotel site, where you can complete your reservation."},
+    {question: "Where does OverNight obtain its data?", answer: "OverNight's data is sourced from HotelBeds, one of the largest hotel data suppliers."},
+    {question: "Why does selecting a date with the calendar not work?", answer: ""},
     {question: "What Tech stack was used to build this app?", answer: " Over Night was build using react with typescript for the front-end, and express for the back-end "},
     {question: "What Tech stack was used to build this app?", answer: " Over Night was build using react with typescript for the front-end, and express for the back-end "},
     {question: "What Tech stack was used to build this app?", answer: " Over Night was build using react with typescript for the front-end, and express for the back-end "}]
@@ -24,14 +24,15 @@ export default function FAQ(){
         return (
         // when button is clicked the div key is added to clicked state
         // if the div key is in clicked array question div is there
-            <div key={index} className="w-2/3  my-4 border flex flex-col border-gray-400 shadow-md items-center rounded">
+            <div key={index} className="w-2/3  my-4 border flex flex-col border-gray-400 shadow-md items-center rounded"
+            onClick={() => setClicked( clicked.includes(index) ? clicked.filter((el : number) => el != index ) : clicked.concat(index) ) }>
 
             {/* question div */}
                 <div className="w-full items-center flex flex-row rounded">
                 {/* question */}
                     <h1  className ='text-2xl text-center my-5  text-black ml-6 '>{el.question}</h1>
                 {/* button */}
-                    <button className="ml-auto mr-5" onClick={() => setClicked( clicked.includes(index) ? clicked.filter((el : number) => el != index ) : clicked.concat(index) ) }>
+                    <button className="ml-auto mr-5">
                         <motion.img 
                         className="h-6 " src={arrow}
                         animate={{
